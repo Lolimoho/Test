4f890a729eff8461bdb4fd5baa5c33fb91b643c0
Lolimoho <meloninwater@outlook.com>
2022-06-13 22:52:58 +0800

    增加GUI测试

---
+++ sample/guiTest1.py
@@ -0,0 +1,9 @@
+'''
+这是GUI的测试1
+'''
+from tkinter import *
+root = Tk()
+root.title('测试窗口')
+w = Label(root, text="Hello Tkinter") 
+w.pack()
+root.mainloop()
--- sample/start.py
+++ sample/start.py
@@ -1,2 +1,3 @@
 import testModule1, testModule2
 print('这是start模块')
+print (testModule1.__doc__)
